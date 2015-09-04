f7bcd4b40778d69b9657a724153470a3159b5ef0
C0rine <Corine_J@MSN.com>
2015-09-04 11:28:27 +0200

    added exercise 1

---
+++ exercise1.java
@@ -0,0 +1,11 @@
+/* 
+   Corine Jacobs
+   10001326
+   Minor Programmeren UvA
+   2015-2016 semester 1
+*/
+
+//  I will skip this exercise since I do not have the
+//  virtual machine installed anymore on my laptop.
+//  I will however make all the other exercises to practice
+//  java. 
